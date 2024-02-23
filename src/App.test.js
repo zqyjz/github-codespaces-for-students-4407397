1549541df7c6bf9d18b98cb01ff35fb89d16fa98
zqyjz <34870848+zqyjz@users.noreply.github.com>
2024-02-23 21:37:50 +0000

    start work

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+//start work
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
